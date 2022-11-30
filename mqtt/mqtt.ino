5a68d3cb9de3c10544d30e55f2642d6c0f4086a8
Zuzanna Cymerman <zuzannacymerman@Zuzannas-MacBook-Air.local>
2022-11-30 01:06:26 +0100

    MQTT arduino with sensor

--- mqtt/mqtt.ino
+++ mqtt/mqtt.ino
@@ -1,9 +1,16 @@
 //#include <UIPEthernet.h>
 #include "PubSubClient.h"
 #include "EthernetENC.h"
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
 #define CLIENT_ID "UnoMQTT"
 #define INTERVAL 1000 // 3 sec delay between publishing
+#define ONE_WIRE_BUS 2
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature.
+DallasTemperature sensors(&oneWire);
 
 int lichtstatus;
 byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
@@ -15,6 +22,8 @@ long previousMillis;
 
 void setup()
 {
+  // Inicjacja czujnika
+  sensors.begin();
   // setup serial communication
   //  Serial.begin(57600);
   Ethernet.begin(mac, ipfixed);
@@ -30,15 +39,17 @@ void loop()
 {
   if (millis() - previousMillis > INTERVAL)
   {
-    sendData();
+    sensors.requestTemperatures();
+    sendData(sensors.getTempCByIndex(0));
     previousMillis = millis();
   }
 }
 
-void sendData()
+void sendData(float temp)
 {
+  char msgBuffer[20];
   if (mqttClient.connect(CLIENT_ID))
   {
-    mqttClient.publish("hal/temp", "dupa");
+    mqttClient.publish("hal/temp", dtostrf(temp, 6, 2, msgBuffer));
   }
 }
